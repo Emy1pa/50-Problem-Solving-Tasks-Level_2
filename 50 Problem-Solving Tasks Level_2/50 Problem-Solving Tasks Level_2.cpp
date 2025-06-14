228bac79e3abd9f9fd69e3512f508f86094f479b
Emy1pa <souaouti.iman@gmail.com>
2025-06-14 16:39:00 +0100

    Solve Problem 1 - Display multiplication table from 1 to 10

--- 50 Problem-Solving Tasks Level_2/50 Problem-Solving Tasks Level_2.cpp
+++ 50 Problem-Solving Tasks Level_2/50 Problem-Solving Tasks Level_2.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
 using namespace std;
 
+void PrintTableHeader() {
+    cout << "\n\n\t\t\t Multiplication Table From 1 to 10 \n";
+    cout << "\n\t";
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << i << "\t";
+    }
+    cout << "\n-------------------------------------------------------------------------------------\n";
+
+}
+string ColumnStringSeperator(int i){
+    if (i < 10)
+        return "   |";
+    else
+        return "  |";
+}
+void PrintTableMultiplication() {
+
+    PrintTableHeader();
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << i << ColumnStringSeperator(i);
+        for (int j = 1; j <= 10; j++)
+        {
+            cout <<"\t" << i * j;
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
-    cout << "Hello World!\n";
+    PrintTableMultiplication();
 }
 
