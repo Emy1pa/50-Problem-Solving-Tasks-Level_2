b422b8970f8dd3ef4cce219d2c0238d3a04d4883
Emy1pa <souaouti.iman@gmail.com>
2025-06-15 15:28:35 +0100

    Solve problem 11: check if a positive number is a palindrome by comparing it with its reversed version and print result

--- 50 Problem-Solving Tasks Level_2/50 Problem-Solving Tasks Level_2.cpp
+++ 50 Problem-Solving Tasks Level_2/50 Problem-Solving Tasks Level_2.cpp
@@ -12,29 +12,31 @@ int ReadPositiveNumber(string Message)
 	} while (Number <= 0);
 	return Number;
 }
-
-int ReverseNumbers(int Number) {
+int ReverseNumber(int Number){
 	int Remainder = 0, Number2 = 0;
-	while (Number >= 1) {
+	while (Number > 0) {
 		Remainder = Number % 10;
 		Number /= 10;
 		Number2 = Number2 * 10 + Remainder;
 	}
 	return Number2;
+}
+bool CheckIfPalindrome(int Number){
+	
+	return Number == ReverseNumber(Number);
+}
+void PrintTheResult(int Number){
+	if (CheckIfPalindrome(Number))
+		cout << "Yes, it is a Palindrome number.";
+	else
+		cout << "No, it ns NOT a Palindrome number.";
+}
 
-}
-void PrintReverseNumberInOrder(int Number) {
-	int Remainder = 0;
-	while (Number > 0) {
-		Remainder = Number % 10;
-		Number /= 10;
-		cout << Remainder << endl;
-	}
-}
+
 int main()
 {
-	PrintReverseNumberInOrder(ReverseNumbers(ReadPositiveNumber("Please enter a positive number ?")));
-
+	//cout << CheckIfPalindrome(ReadPositiveNumber("Please enter a number ?"));
+	PrintTheResult(ReadPositiveNumber("Please enter a positive number ?"));
 	return 0;
 }
 
