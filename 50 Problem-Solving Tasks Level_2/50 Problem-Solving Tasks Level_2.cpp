4e788a94cd8afa8bc3d1682d91ef3496edfbae5a
Emy1pa <souaouti.iman@gmail.com>
2025-06-17 10:32:23 +0100

    Solve problem 22 by reading numbers into an array, printing them, and counting how many times a given number appears.

--- 50 Problem-Solving Tasks Level_2/50 Problem-Solving Tasks Level_2.cpp
+++ 50 Problem-Solving Tasks Level_2/50 Problem-Solving Tasks Level_2.cpp
@@ -2,50 +2,50 @@
 #include <cstdlib>
 using namespace std;
 
-int GetRandomNumber(int From, int To) {
-	int Number = rand() % (To - From + 1) + From;
+int ReadPositiveNumber(string Message){
+	int Number;
+	do
+	{
+		cout << Message << endl;
+		cin >> Number;
+	} while (Number < 0);
 	return Number;
 }
 
-int askKeyCount() {
-	int count;
-	cout << "Please enter how many Keys you want to generate ? \n";
-	cin >> count;
-	return count;
-}
-string generateKeySegment() {
-	string segment;
-	for (int i = 1; i <= 4; i++)
+void ReadArrayElements(int Arr[], int &Length) {
+	Length = ReadPositiveNumber("Please how many Inputs you want to enter ? ");
+	for (int i = 0; i < Length; i++)
 	{
-		segment += char(GetRandomNumber(65, 90));
+		cout << "Please enter element " << (i + 1) << endl;
+		cin >> Arr[i];
 	}
-	return segment;
+}
+void PrintArrayElements(int Arr[], int Length) {
+	for (int i = 0; i < Length; i++)
+	{
+		cout << "Element [" << (i + 1) << "] : " << Arr[i] << endl;
+	}
+}
+int PrintTheCheckedNumber(int Arr[], int Length, int NumberToCheck) {
+	int Counter = 0;
+	for (int i = 0; i < Length; i++)
+	{
+		if (NumberToCheck == Arr[i])
+			Counter++;
+	}
+	return Counter;
 }
 
-
-string generateFullKey() {
-	string fullKey;
-	for (int i = 1; i <= 4; i++)
-	{
-		fullKey += generateKeySegment();
-		if (i < 4) {
-			fullKey += "-";
-		}
-	}
-	return fullKey;
-}
-void displayGeneratedKeys() {
-	int numberOfKeys = askKeyCount();
-	for (int i = 1; i <= numberOfKeys; i++) {
-		cout << "Key [" << i << "] : " << generateFullKey() << endl;
-	}
-}
 
 
 int main()
 {
-	srand((unsigned)time(NULL));
-	displayGeneratedKeys();
+	int Arr[100], Length, NumberToCheck, RepeatedTimes;
+	ReadArrayElements(Arr, Length);
+	PrintArrayElements(Arr, Length);
+	NumberToCheck = ReadPositiveNumber("\nPlease enter a number to check ?");
+	RepeatedTimes = PrintTheCheckedNumber(Arr, Length, NumberToCheck);
+	cout << NumberToCheck << " is repeated " << RepeatedTimes << " time(s) \n";
 	return 0;
 }
 
