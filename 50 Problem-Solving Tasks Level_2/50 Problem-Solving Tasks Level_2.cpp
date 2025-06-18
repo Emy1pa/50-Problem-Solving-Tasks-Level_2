21bf5ff1b84e8e48b9de49b3870969b0b17a3528
Emy1pa <souaouti.iman@gmail.com>
2025-06-18 13:55:16 +0100

    Solve Problem 31: Shuffle array elements randomly using swapping

--- 50 Problem-Solving Tasks Level_2/50 Problem-Solving Tasks Level_2.cpp
+++ 50 Problem-Solving Tasks Level_2/50 Problem-Solving Tasks Level_2.cpp
@@ -14,34 +14,36 @@ int ReadPositiveNumber(string Message)
 	return Number;
 }
 
-
+void SwapNumbers(int &Num1, int &Num2){
+	int Temp = Num1;
+	Num1 = Num2;
+	Num2 = Temp;
+}
 
 int GenerateRandomNumber(int From, int To){
 	int randomNumber = rand() % (To - From + 1) + From;
 	return randomNumber;
 }
 
-void FillArrayWithRandomNumbers(int Arr[100], int &arrLength) {
-	for (int i = 0; i < arrLength; i++)
+void FillArrayWithNumsFrom1ToN(int Arr[100], int &arrLength) {
+	for (int i = 0; i <= arrLength; i++)
 	{
-		Arr[i] = GenerateRandomNumber(1, 100);
+		Arr[i] = i + 1;
 	}
 	
 }
-
+void DisplayArrayElementsAfterShuffle(int Arr[100], int arrLength) {
+	for (int i = 0; i < arrLength; i++)
+	{
+		SwapNumbers(Arr[GenerateRandomNumber(1, arrLength) - 1], Arr[GenerateRandomNumber(1, arrLength) - 1]);
+	}
+}
 
 
 void PrintArray(int Arr[100], int arrLength) {
 	for (int i = 0; i < arrLength; i++)
 		cout << Arr[i] << " ";
 	cout << "\n";
-}
-void CopyArrayInReverse(int ArrSource[100], int ArrDestination[100], int& arrLength){
-	for (int i = 0; i <arrLength; i++)
-	{
-		ArrDestination[i] = ArrSource[arrLength - (i + 1)];
-	}
-	cout << endl;
 }
 
 
@@ -52,20 +54,18 @@ int main()
 {
 	srand((unsigned)time(NULL));
 
-	int arr[100], arrLength, arr2[100];
+	int arr[100], arrLength;
 	arrLength = ReadPositiveNumber("Please enter number of elements ?");
 
-	FillArrayWithRandomNumbers(arr, arrLength);
+	FillArrayWithNumsFrom1ToN(arr, arrLength);
 
-	cout << "Array 1 elements: ";
+	cout << "Array elements before shuffle: ";
 	PrintArray(arr, arrLength);
-
-	CopyArrayInReverse(arr, arr2, arrLength);
-	cout << "Array 2 elements after copying array 1 in reversed order: ";
-	PrintArray(arr2, arrLength);
 	cout << endl;
 	
-
+	cout << "Array elements after shuffle: ";
+	DisplayArrayElementsAfterShuffle(arr, arrLength);
+	PrintArray(arr, arrLength);
 	cout << endl;
 	return 0;
 }
