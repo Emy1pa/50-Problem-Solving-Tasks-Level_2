35d1966dabb8add0083dc7b01a24151a3e8b2bba
Emy1pa <souaouti.iman@gmail.com>
2025-06-18 10:02:36 +0100

    Solve Problem 30: generate two random arrays, sum them element-wise, and print the resulting values without modifying original arrays

--- 50 Problem-Solving Tasks Level_2/50 Problem-Solving Tasks Level_2.cpp
+++ 50 Problem-Solving Tasks Level_2/50 Problem-Solving Tasks Level_2.cpp
@@ -3,26 +3,8 @@
 #include <cmath>
 using namespace std;
 
-//int ReadPositiveNumber(string Message){
-//	int Number;
-//	do
-//	{
-//		cout << Message << endl;
-//		cin >> Number;
-//	} while (Number < 0);
-//	return Number;
-//}
-enum enPrimeNotPrime { Prime = 1, NotPrime = 2 };
 
-enPrimeNotPrime CheckPrimeNumber(int Number){
-	int M = round(Number / 2);
-	for (int i = 2; i <= M; i++)
-	{
-		if (Number % i == 0)
-			return enPrimeNotPrime::NotPrime;
-	}
-	return enPrimeNotPrime::Prime;
-}
+
 
 int GenerateRandomNumber(int From, int To){
 	int randomNumber = rand() % (To - From + 1) + From;
@@ -48,14 +30,12 @@ void PrintArray(int Arr[100], int arrLength) {
 	cout << "\n";
 }
 
-void PrimeNumbersCopy(int ArrSource[100], int ArrDestination[100], int arrLength, int &PrimeCount) {
-	PrimeCount = 0;
+void PrintSumOfBothArray(int ArrSource[100], int ArrDestination[100], int arrLength){
+	int Sum = 0;
 	for (int i = 0; i < arrLength; i++)
 	{
-		if (CheckPrimeNumber(ArrSource[i]) == enPrimeNotPrime::Prime) {
-			ArrDestination[PrimeCount] = ArrSource[i];
-			PrimeCount++;
-		}
+		Sum = ArrDestination[i] + ArrSource[i];
+		cout << Sum << " ";
 	}
 }
 
@@ -70,14 +50,20 @@ int main()
 	
 	FillRandomNumbersToArray(arr, arrLength);
 	
-	int Arr2[100], PrimeCount;
-	PrimeNumbersCopy(arr, Arr2, arrLength, PrimeCount);
-
+	int Arr2[100];
 	cout << "Array 1 Elements : ";
 	PrintArray(arr, arrLength);
+	cout << endl;
 
-	cout << "Prime Numbers in Array2: ";
-	PrintArray(Arr2, PrimeCount);
+	FillRandomNumbersToArray(Arr2, arrLength);
+	cout << "Array 2 Elements: ";
+	PrintArray(Arr2, arrLength);
+	cout << endl;
+
+	cout << "Sum of array 1 and array 2 elements: ";
+	PrintSumOfBothArray(arr, Arr2, arrLength);
+	cout << endl;
+
 	return 0;
 }
 
