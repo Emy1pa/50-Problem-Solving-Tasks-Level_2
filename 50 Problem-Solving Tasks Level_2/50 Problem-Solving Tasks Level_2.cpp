c921521c79a07bb65ec5ffe2161ff7ee868aff8c
Emy1pa <souaouti.iman@gmail.com>
2025-06-19 10:24:54 +0100

    Solve Problem 35: Search number in array using modular functions

--- 50 Problem-Solving Tasks Level_2/50 Problem-Solving Tasks Level_2.cpp
+++ 50 Problem-Solving Tasks Level_2/50 Problem-Solving Tasks Level_2.cpp
@@ -15,7 +15,12 @@ void FillArrayWithRandomNumbers(int arr[100], int& arrLength) {
 			arr[i] = RandomNumber(1, 100);}void PrintRandomNumbers(int arr[100], int arrLength) {	for (int i = 0; i < arrLength; i++)
 	{
 		cout << arr[i] << " ";
-	}}void PrintSearchedNumber(int arr[100], int arrLength, int &SearchedNumber) {	cout << "\nPlease enter a number to search for ? \n";	cin >> SearchedNumber;	bool found = false;	for (int i = 0; i < arrLength; i++ ) {		if (arr[i] == SearchedNumber) {			cout << "The number you are looking for is : " << SearchedNumber << endl;			cout << "The number found at position : " << i << endl;			cout << "The number found its order : " << (i + 1) << endl;			found = true; 			break;		}		}	if (!found) {		cout << "\nThe number you are looking for is : " << SearchedNumber << endl;		cout << "The number is not found :-( " << endl;	}}int main(){	srand((unsigned)time(NULL));
+	}}void PrintSearchedNumber(int arr[100], int arrLength, int &SearchedNumber) {	cout << "\nPlease enter a number to search for ? \n";	cin >> SearchedNumber;	bool found = false;	for (int i = 0; i < arrLength; i++ ) {		if (arr[i] == SearchedNumber) {			cout << "The number you are looking for is : " << SearchedNumber << endl;			cout << "The number found at position : " << i << endl;			cout << "The number found its order : " << (i + 1) << endl;			found = true; 			break;		}		}	if (!found) {		cout << "\nThe number you are looking for is : " << SearchedNumber << endl;		cout << "The number is not found :-( " << endl;	}}short FindNumberPositionInArray(int Number, int arr[100], int arrLength) {	for (int i = 0; i < arrLength; i++)
+	{
+		if (arr[i] == Number) 
+			return i;
+		
+	}	return -1;}bool FindSearchedNumber(int Number , int arr[100], int arrLength) {	if (FindNumberPositionInArray(Number, arr, arrLength) == -1)		return false;	else		return true;}int ReadNumber() {	int Number; 	cout << "\nPlease enter a number to search for ? \n";	cin >> Number;	return Number;}int main(){	srand((unsigned)time(NULL));
 	int arr[100], arrLength, searchedNumber;
 	
 	FillArrayWithRandomNumbers(arr, arrLength);
@@ -23,4 +28,7 @@ void FillArrayWithRandomNumbers(int arr[100], int& arrLength) {
 	PrintRandomNumbers(arr, arrLength);
 	cout << endl;
 
-	PrintSearchedNumber(arr, arrLength, searchedNumber);	return 0;}
+	int Number = ReadNumber();
+	cout << "The number you're looking for is: " << Number << endl;
+
+	bool NumberFound = FindSearchedNumber(Number, arr, arrLength);	if (NumberFound) {		cout << "\nYes, the number is found :-) \n";	}	else {		cout << "No, the number is not found :-(" << endl;	}	return 0;}
