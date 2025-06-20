9568154b0bc74c608b9fe544dabfd6e2006bf1c4
Emy1pa <souaouti.iman@gmail.com>
2025-06-20 11:48:27 +0100

    Solving problem 44: count the number of positive numbers in a randomly generated array

--- 50 Problem-Solving Tasks Level_2/50 Problem-Solving Tasks Level_2.cpp
+++ 50 Problem-Solving Tasks Level_2/50 Problem-Solving Tasks Level_2.cpp
@@ -2,13 +2,13 @@
 #include <cstdlib>
 using namespace std;int RandomNumber(int From, int To){	int RandomNumber = rand() % (To - From + 1) + From;	return RandomNumber;}void FillArrayWithRandomNumbers(int arr[100], int &arrLength) {	cout << "How many elements do you want to enter ? \n";	cin >> arrLength;	for (int i = 0; i < arrLength; i++)
 	{
-		arr[i] = RandomNumber(1, 100);
+		arr[i] = RandomNumber(-100, 100);
 	}}void PrintArray(int arr[100], int arrLength) {	for (int i = 0; i < arrLength; i++)
 	{
 		cout << arr[i] << " ";
-	}	cout << endl;}int EvenNumbersInArrayCount(int arr[100], int arrLength) {	int Counter = 0;	for (int i = 0; i < arrLength; i++)
+	}	cout << endl;}int PositiveNumberCount(int arr[100], int arrLength) {	int Counter = 0;	for (int i = 0; i < arrLength; i++)
 	{
-		if (arr[i] % 2 == 0) {
+		if (arr[i] > 0) {
 			Counter++;
 		}
 	}	return Counter;}int main() {	srand((unsigned)time(NULL));
@@ -19,4 +19,4 @@ using namespace std;int RandomNumber(int From, int To){	int RandomNumber = ra
 
 	cout << "Array Elements : " << endl;
 	PrintArray(arr, arrLength);
-	cout << endl; 	cout << "Even Numbers Count is: " << EvenNumbersInArrayCount(arr, arrLength) << endl;		return 0;}
+	cout << endl; 	cout << "Positive Numbers Count is: " << PositiveNumberCount(arr, arrLength) << endl;		return 0;}
