7d66bbe6a4a5b07c61ec5684f298c8a9ff30bab8
Emy1pa <souaouti.iman@gmail.com>
2025-06-20 11:31:37 +0100

    Solving problem 42: count the number of odd numbers in a randomly generated array

--- 50 Problem-Solving Tasks Level_2/50 Problem-Solving Tasks Level_2.cpp
+++ 50 Problem-Solving Tasks Level_2/50 Problem-Solving Tasks Level_2.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 #include <cstdlib>
-using namespace std;void FillArray(int arr[100], int &arrLength) {		arrLength = 6;	arr[0] = 10;	arr[1] = 20;	arr[2] = 30;	arr[3] = 30;	arr[4] = 20;	arr[5] = 10;}void PrintArray(int arr[100], int arrLength) {	for (int i = 0; i < arrLength; i++)
+using namespace std;int RandomNumber(int From, int To){	int RandomNumber = rand() % (To - From + 1) + From;	return RandomNumber;}void FillArrayWithRandomNumbers(int arr[100], int &arrLength) {	cout << "How many elements do you want to enter ? \n";	cin >> arrLength;	for (int i = 0; i < arrLength; i++)
+	{
+		arr[i] = RandomNumber(1, 100);
+	}}void PrintArray(int arr[100], int arrLength) {	for (int i = 0; i < arrLength; i++)
 	{
 		cout << arr[i] << " ";
-	}	cout << endl;}bool IsPalindromeArrayCheck(int arr[100], int arrLength) {	for (int i = 0; i < arrLength; i++)
+	}	cout << endl;}int OddNumbersInArrayCount(int arr[100], int arrLength) {	int Counter = 0;	for (int i = 0; i < arrLength; i++)
 	{
-		if (arr[i] != arr[arrLength - 1 - i]) {
-			return false;
+		if (arr[i] % 2 != 0) {
+			Counter++;
 		}
-	}	return true;}int main() {	srand((unsigned)time(NULL));
+	}	return Counter;}int main() {	srand((unsigned)time(NULL));
 	
 	int arr[100], arrLength;
 
-	FillArray(arr, arrLength);
+	FillArrayWithRandomNumbers(arr, arrLength);
 
 	cout << "Array Elements : " << endl;
 	PrintArray(arr, arrLength);
-	cout << endl; 	if (IsPalindromeArrayCheck(arr, arrLength)) {		cout << "Yes array is Palindrome \n";	}	else {		cout << "No array is not Palindrome \n";	}	return 0;}
+	cout << endl; 	cout << "Odd Numbers Count is: " << OddNumbersInArrayCount(arr, arrLength) << endl;		return 0;}
