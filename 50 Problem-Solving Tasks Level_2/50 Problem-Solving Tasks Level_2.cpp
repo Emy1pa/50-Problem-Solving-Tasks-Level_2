97a467c3adef9fc1a03d86517762451631c3235e
Emy1pa <souaouti.iman@gmail.com>
2025-06-20 12:03:10 +0100

    Solving problem 46: implement custom absolute value function correctly

--- 50 Problem-Solving Tasks Level_2/50 Problem-Solving Tasks Level_2.cpp
+++ 50 Problem-Solving Tasks Level_2/50 Problem-Solving Tasks Level_2.cpp
@@ -1,22 +1,6 @@
 #include <iostream>
-#include <cstdlib>
-using namespace std;int RandomNumber(int From, int To){	int RandomNumber = rand() % (To - From + 1) + From;	return RandomNumber;}void FillArrayWithRandomNumbers(int arr[100], int &arrLength) {	cout << "How many elements do you want to enter ? \n";	cin >> arrLength;	for (int i = 0; i < arrLength; i++)
-	{
-		arr[i] = RandomNumber(-100, 100);
-	}}void PrintArray(int arr[100], int arrLength) {	for (int i = 0; i < arrLength; i++)
-	{
-		cout << arr[i] << " ";
-	}	cout << endl;}int NegativeNumberCount(int arr[100], int arrLength) {	int Counter = 0;	for (int i = 0; i < arrLength; i++)
-	{
-		if (arr[i] < 0) {
-			Counter++;
-		}
-	}	return Counter;}int main() {	srand((unsigned)time(NULL));
+#include <cmath>
+//#include <cstdlib>
+using namespace std;float ReadAbs() {	float AbsNumber;	cout << "Please enter a number ? \n";	cin >> AbsNumber;	return AbsNumber;}int MyAbsoluteFunction(float AbsNumber){	if (AbsNumber < 0) {		return AbsNumber * -1;	}	return AbsNumber;}int main() {	//srand((unsigned)time(NULL));
 	
-	int arr[100], arrLength;
-
-	FillArrayWithRandomNumbers(arr, arrLength);
-
-	cout << "Array Elements : " << endl;
-	PrintArray(arr, arrLength);
-	cout << endl; 	cout << "Negative Numbers Count is: " << NegativeNumberCount(arr, arrLength) << endl;		return 0;}
+	float AbsNumber = ReadAbs();	cout << "My abs Result : " << MyAbsoluteFunction(AbsNumber) << endl;	cout << "C++ abs Result : " << abs(AbsNumber) << endl;	return 0;}
