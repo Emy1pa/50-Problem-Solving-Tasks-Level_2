1946c60767172ed49dcb4dd1d68575dc1a3fb5dd
Emy1pa <souaouti.iman@gmail.com>
2025-06-20 15:07:34 +0100

    Solve problem 47 by implementing custom rounding logic

--- 50 Problem-Solving Tasks Level_2/50 Problem-Solving Tasks Level_2.cpp
+++ 50 Problem-Solving Tasks Level_2/50 Problem-Solving Tasks Level_2.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <cmath>
 //#include <cstdlib>
-using namespace std;float ReadAbs() {	float AbsNumber;	cout << "Please enter a number ? \n";	cin >> AbsNumber;	return AbsNumber;}int MyAbsoluteFunction(float AbsNumber){	if (AbsNumber < 0) {		return AbsNumber * -1;	}	return AbsNumber;}int main() {	//srand((unsigned)time(NULL));
-	
-	float AbsNumber = ReadAbs();	cout << "My abs Result : " << MyAbsoluteFunction(AbsNumber) << endl;	cout << "C++ abs Result : " << abs(AbsNumber) << endl;	return 0;}
+using namespace std;float ReadNumber() {	float Number;	cout << "Please enter a number ? \n";	cin >> Number;	return Number;}float RoundOfTheNumber(float Number){	int IntPart = int(Number);	cout << IntPart << endl;	float DecimalPart = Number - IntPart;	if (DecimalPart >= 0.5) {			return IntPart + 1;	}	else if (DecimalPart <= -0.5) {		return IntPart - 1;	}	else {		return IntPart;	}}int main() {	//srand((unsigned)time(NULL));
+	float Number = ReadNumber();
+
+	cout << "My Round Result: " << RoundOfTheNumber(Number) << endl;
+	cout << "C++ Round Result: " << round(Number) << endl;
+		return 0;}
